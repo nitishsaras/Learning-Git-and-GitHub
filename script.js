bf6ce2cce36fe8ce27bfe301f82b164027612457
nitishkumar <mailonnitishkumar@gmail.com>
2025-02-25 02:07:29 +0530

    npm package manager

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+import axios from 'axios'
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then((response) => {
+    console.log(response.data);
+  })
+
+// setTimeout(() => {console.log('hello world')},2000)
